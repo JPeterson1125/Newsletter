08ea8635300c51aaf3c2ca2ff1301380427660d0
Jack Peterson <jpetey1125@gmail.com>
2023-02-14 13:22:31 -0600

    Change path back

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "signup.html")
+  res.sendFile(__dirname + "/signup.html")
 });
 
 app.post("/", function(req, res) {
